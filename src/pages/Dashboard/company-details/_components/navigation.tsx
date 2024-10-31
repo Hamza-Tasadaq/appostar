aa58304f54f2ee1a4b0b63cbe883d03fc0aad682
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2024-10-31 20:37:28 +0500

    Fixed

--- src/pages/Dashboard/company-details/_components/navigation.tsx
+++ src/pages/Dashboard/company-details/_components/navigation.tsx
@@ -6,7 +6,7 @@ const Navigation = () => {
         <ul className='list-unstyled navigation-list mb-0 rounded-top d-flex justify-content-center justify-content-md-start align-items-center gap-4'>
             <li>
                 <Link to={""} >
-                    <i className='ri me-1 ri-home-line fs-4'></i>
+                    <i className='ri me-md-1 ri-home-line fs-4'></i>
                     <span className='d-none d-md-inline'>
                         Home
                     </span>
@@ -14,7 +14,7 @@ const Navigation = () => {
             </li>
             <li>
                 <Link to={""}>
-                    <i className='ri me-1 ri-chat-3-line fs-4'></i>
+                    <i className='ri me-md-1 ri-chat-3-line fs-4'></i>
                     <span className='d-none d-md-inline'>
                         Review
                     </span>
@@ -22,9 +22,17 @@ const Navigation = () => {
             </li>
             <li>
                 <Link to={""}>
-                    <i className='ri me-1 ri-chat-3-line fs-4'></i>
+                    <i className='ri me-md-1 ri-chat-3-line fs-4'></i>
                     <span className='d-none d-md-inline'>
                         Chat
+                    </span>
+                </Link>
+            </li>
+            <li className='d-inline d-md-none'>
+                <Link to={""}>
+                    <i className='ri me-md-1 ri-list-check-2 fs-4'></i>
+                    <span className='d-none d-md-inline'>
+                        List
                     </span>
                 </Link>
             </li>
