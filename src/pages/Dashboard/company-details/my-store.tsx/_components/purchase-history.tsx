8a02da2f373043b4194adade301c6c6a27b9e35c
Hamza-Tasadaq <hamzatasadaq01@gmail.com>
2024-11-05 22:21:23 +0500

    Improved

--- src/pages/Dashboard/company-details/my-store.tsx/_components/purchase-history.tsx
+++ src/pages/Dashboard/company-details/my-store.tsx/_components/purchase-history.tsx
@@ -15,53 +15,82 @@ const PurchaseHistory = () => {
                                 <h2 className='mb-0'>Order#45</h2>
                                 <Badge color='warning'>Processing</Badge>
                             </CardHeader>
+
                             <CardBody>
-                                <ul className='list-unstyled'>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Order date</h3>
-                                        <h3>13/10/2024</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Delivery date</h3>
-                                        <h3>13/10/2024</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Order Amount</h3>
-                                        <h3>45,00€</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3 className='grand-total'>Grand Total</h3>
-                                        <h3 className='grand-total'>45,00€</h3>
-                                    </li>
-                                </ul>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Order date:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">13/10/2024</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Delivery date:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">13/10/2024</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Order Amount:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">45,00€</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Grand Total:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">45,00€</h6>
+                                    </div>
+                                </div>
                             </CardBody>
                         </Card>
                     </li>
                     <li>
-                        <Card className='purchase-card mb-2'>
+                        <Card className='purchase-card mb-2 active'>
                             <CardHeader className='d-flex align-items-center justify-content-between'>
                                 <h2 className='mb-0'>Order#45</h2>
                                 <Badge color='success'>Complete</Badge>
                             </CardHeader>
                             <CardBody>
-                                <ul className='list-unstyled'>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Order date</h3>
-                                        <h3>13/10/2024</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Delivery date</h3>
-                                        <h3>13/10/2024</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Order Amount</h3>
-                                        <h3>45,00€</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3 className='grand-total'>Grand Total</h3>
-                                        <h3 className='grand-total'>45,00€</h3>
-                                    </li>
-                                </ul>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Order date:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">13/10/2024</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Delivery date:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">13/10/2024</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Order Amount:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">45,00€</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Grand Total:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">45,00€</h6>
+                                    </div>
+                                </div>
                             </CardBody>
                         </Card>
                     </li>
@@ -72,25 +101,40 @@ const PurchaseHistory = () => {
                                 <h2 className='mb-0'>Order#45</h2>
                                 <Badge color='danger'>Refunded</Badge>
                             </CardHeader>
+
                             <CardBody>
-                                <ul className='list-unstyled'>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Order date</h3>
-                                        <h3>13/10/2024</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Delivery date</h3>
-                                        <h3>13/10/2024</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3>Order Amount</h3>
-                                        <h3>45,00€</h3>
-                                    </li>
-                                    <li className='d-flex align-items-center justify-content-between'>
-                                        <h3 className='grand-total'>Grand Total</h3>
-                                        <h3 className='grand-total'>45,00€</h3>
-                                    </li>
-                                </ul>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Order date:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">13/10/2024</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Delivery date:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">13/10/2024</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Order Amount:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">45,00€</h6>
+                                    </div>
+                                </div>
+                                <div className="d-flex align-items-center mb-2">
+                                    <div className="flex-shrink-0">
+                                        <p className="text-muted mb-0">Grand Total:</p>
+                                    </div>
+                                    <div className="flex-grow-1 ms-2">
+                                        <h6 className="mb-0">45,00€</h6>
+                                    </div>
+                                </div>
                             </CardBody>
                         </Card>
                     </li>
